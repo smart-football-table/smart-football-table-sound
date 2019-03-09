71a06f2aa08f59e368729d05288f7ea7c0a84807
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-03-09 17:13:34 +0100

    topic prefixed

--- play_on_mqtt.sh
+++ play_on_mqtt.sh
@@ -9,4 +9,4 @@ while read msg;
 do
    score=`echo $msg | jq -j .score[0,1]`
    [ "$score" != "00" ] && play "${FILES[RANDOM % ${#FILES[@]}]}" &
-done < <(mosquitto_sub -h "$MQTTHOST" -p "$MQTTPORT" -t score)
+done < <(mosquitto_sub -h "$MQTTHOST" -p "$MQTTPORT" -t table/score)
