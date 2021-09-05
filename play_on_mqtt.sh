d96c269efe3c6936d60da9e7e8f0110223094890
Peter Fichtner <github.07.fichtner@0sg.net>
2021-09-06 00:41:31 +0200

    fixed syntax error

--- play_on_mqtt.sh
+++ play_on_mqtt.sh
@@ -5,5 +5,5 @@
 
 while read msg;
 do
-   find sounds/ -type f -print0 | shuf -zn1 | xargs -0 play &`
+   find sounds/ -type f -print0 | shuf -zn1 | xargs -0 play &
 done < <(mosquitto_sub -h "$MQTTHOST" -p "$MQTTPORT" -t team/scored)
